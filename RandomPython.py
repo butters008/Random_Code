c88bebab876ab6d08e24cba8629167c31eecdb9f
Butters <k.butterfield08@gmail.com>
2021-03-22 01:45:21 -0500

    ad
    sad

--- RandomPython.py
+++ RandomPython.py
@@ -2,5 +2,6 @@ print("This is for me late at night wanting to code")
 print("and not on anything important!")
 
 #This is going to be...amazing
+#This is going to be...amazing
 name = input("Please enter your name ")
 print(name)
