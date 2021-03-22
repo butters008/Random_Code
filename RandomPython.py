14b22bdcc57c0e8fd60203cbe6d6c2782e37ce25
Butters <k.butterfield08@gmail.com>
2021-03-22 01:41:12 -0500

    Create RandomPython.py

---
+++ RandomPython.py
@@ -0,0 +1,6 @@
+print("This is for me late at night wanting to code")
+print("and not on anything important!")
+
+#This is going to be...amazing
+name = input("Please enter your name ")
+print(name)
