d3cda3c39a6563bf50c197fdebfb4a81c6f93343
Butters <k.butterfield08@gmail.com>
2021-03-25 01:14:56 -0500

    Goodnight Push
    Just pushing the little code I have for this random night project

--- RandomPython.py
+++ RandomPython.py
@@ -1,6 +1,24 @@
-print("This is for me late at night wanting to code")
-print("and not on anything important!")
+#Author: Keith Butterfield
+#This is late night coding for me to let loose and just code what I want without it being tied to anything
+#Although some code might make its way into future or even current projects, but for the most part
+#This is for me having fun and wanting to code!
+#ALSO - This is me trying to remember how to do things!
 
-#This is going to be...amazing
-name = input("Please enter your name ")
-print(name)
+#We are going to put all global or instance vars here
+tempVar = 0
+
+#We are going to define all functions here
+def function1():
+    print("We are in function 1!")
+
+def main():
+    print("This is going to be epic!")
+    name = input("Please enter your name ")
+    print(name)
+    function1()
+
+
+
+
+#This is where we call the functions - main() will always be at bottom
+main()
