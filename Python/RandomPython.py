e76d6b5a01eaf214b7912925896b0902d46b6d87
Butters <k.butterfield08@gmail.com>
2023-02-15 14:16:50 -0600

    Python debugging

--- Python/RandomPython.py
+++ Python/RandomPython.py
@@ -1,16 +1,19 @@
-#Author: Keith Butterfield
-#This is late night coding for me to let loose and just code what I want without it being tied to anything
-#Although some code might make its way into future or even current projects, but for the most part
-#This is for me having fun and wanting to code!
-#ALSO - This is me trying to remember how to do things!
+# Author: Keith Butterfield
+# This is late night coding for me to let loose and just code what I want without it being tied to anything
+# Although some code might make its way into future or even current projects, but for the most part
+# This is for me having fun and wanting to code!
+# ALSO - This is me trying to remember how to do things!
 
-#We are going to put all global or instance vars here
+# We are going to put all global or instance vars here
 tempVar = 0
 
-#We are going to define all functions here
+
+# We are going to define all functions here
 def function1():
     print("We are in function 1!")
 
+
+# Last function on the list
 def main():
     print("This is going to be epic!")
     name = input("Please enter your name ")
@@ -18,7 +21,5 @@ def main():
     function1()
 
 
-
-
-#This is where we call the functions - main() will always be at bottom
+# This is where we call the functions - main() will always be at bottom
 main()
